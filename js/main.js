66ec3e0ea31516d7ceac9370fe67a29441d63252
Jakub Kicek <j.kicek@gmail.com>
2018-08-22 20:40:29 +0200

    init&parallax

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+//    let windowWidth = $(window).width();
+//    console.log(windowWidth);
+    
+   console.log("dziala");
+    window.addEventListener("scroll",function(e){
+        
+        console.log("fssss");
+        let top = this.pageYOffset;
+        let layers = $('.paralax');
+        let layer, speed, yPos;
+        for(let i=0;i<layers.length;i++){
+            layer=layers[i];
+            speed=$(layer).attr("data-speed");
+            yPos = -(top*speed/100);
+            $(layer).attr('style','transform: translate3d(0px, ' + yPos + 'px, 0px)');
+        }
+    })
+    
+    
+    
+})

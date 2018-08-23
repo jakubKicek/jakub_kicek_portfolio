cd9faca5b2ef3c4df2acfef4e8142edc7eeb7807
Jakub Kicek <j.kicek@gmail.com>
2018-08-24 01:54:28 +0200

    about i twarz

--- js/main.js
+++ js/main.js
@@ -1,22 +1,25 @@
-$(document).ready(function(){
-//    let windowWidth = $(window).width();
-//    console.log(windowWidth);
-    
-   console.log("dziala");
-    window.addEventListener("scroll",function(e){
-        
-        console.log("fssss");
+$(document).ready(function () {
+    window.addEventListener("scroll", function (e) {
+
         let top = this.pageYOffset;
         let layers = $('.paralax');
         let layer, speed, yPos;
-        for(let i=0;i<layers.length;i++){
-            layer=layers[i];
-            speed=$(layer).attr("data-speed");
-            yPos = (top*speed/100);
-            $(layer).attr('style','transform: translate3d(0px, ' + yPos + 'px, 0px)');
+        for (let i = 0; i < layers.length; i++) {
+            layer = layers[i];
+            speed = $(layer).attr("data-speed");
+            yPos = (top * speed / 100);
+            $(layer).attr('style', 'transform: translate3d(0px, ' + yPos + 'px, 0px)');
+        }
+
+        let nav = $('#main-nav');
+        let windowHeight = this.innerHeight;
+        if (top >= windowHeight*0.3) {
+            nav.addClass('amWhite');
+        } else {
+            nav.removeClass('amWhite');
         }
     })
-    
-    
-    
+
+
+
 })
